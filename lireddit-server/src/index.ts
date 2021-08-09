0c237cb621ac1935e0790d2bedccdc8247c5142f
iuliancarnaru <iulian.carnaru@gmail.com>
2021-08-09 23:14:06 +0100

    adding express and apollo, first resolver and first migration

---
+++ lireddit-server/src/index.ts
@@ -0,0 +1,30 @@
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+// import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+
+async function main() {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () =>
+    console.log(`  ✅ Server running on: http://localhost:4000`)
+  );
+}
+
+main().catch((err) => console.error(err));
