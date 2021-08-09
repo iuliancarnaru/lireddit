b129f679922e88dd5c772797f61c424f91db34b2
iuliancarnaru <iulian.carnaru@gmail.com>
2021-08-09 23:38:02 +0100

    creating CRUD for posts

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,11 +1,12 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
-import { __prod__ } from './constants';
-// import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
+import { __prod__ } from './constants';
 
 async function main() {
   const orm = await MikroORM.init(microConfig);
@@ -14,9 +15,10 @@ async function main() {
   const app = express();
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
 
   await apolloServer.start();
