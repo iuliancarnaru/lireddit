7e99805e0ca287ea1d1759f89893620bef2e3fbd
iuliancarnaru <iulian.carnaru@gmail.com>
2021-08-12 22:47:13 +0100

    register page

--- lireddit-web/src/components/InputField.tsx
+++ lireddit-web/src/components/InputField.tsx
@@ -10,17 +10,25 @@ import { useField } from 'formik';
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
-  placeholder?: string;
 };
 
-const InputField: React.FC<InputFieldProps> = (props) => {
+const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
   const [field, { error }] = useField(props);
 
   return (
     // "!!" converts to a boolean (default string)
     <FormControl isInvalid={!!error}>
-      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-      <Input {...field} id={field.name} placeholder={props.placeholder} />
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input
+        {...props}
+        {...field}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Formik, Form } from 'formik';
 import Wrapper from '../components/Wrapper';
 import InputField from '../components/InputField';
+import { Box, Button } from '@chakra-ui/react';
 
 interface RegisterProps {}
 
@@ -14,19 +15,29 @@ const Register: React.FC<RegisterProps> = ({}) => {
           console.log(values);
         }}
       >
-        {({ values, handleChange }) => (
+        {({ isSubmitting }) => (
           <Form>
             <InputField
               name="username"
               label="Username"
               placeholder="username"
             />
-            <InputField
-              name="password"
-              label="Password"
-              placeholder="password"
-              type='password'
-            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                label="Password"
+                placeholder="password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              colorScheme="teal"
+              isLoading={isSubmitting}
+            >
+              Register
+            </Button>
           </Form>
         )}
       </Formik>
